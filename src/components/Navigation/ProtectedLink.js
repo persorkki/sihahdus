2e5ef7e0386d459c5c385440863838e74de0c7cf
Persorkki <persorkki@protonmail.com>
2023-03-01 15:55:06 +0200

    this time actually moved ProtectedLink into its own file

--- src/components/Navigation/ProtectedLink.js
+++ src/components/Navigation/ProtectedLink.js
@@ -8,6 +8,6 @@ export default function ProtectedLink({ session, route, checkLoggedIn, checkRout
                     {children}
                 </li>
             </Link>
-        </>
+        </> 
     )
 }
