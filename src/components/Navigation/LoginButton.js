1d1cb169a20ec2c6a7030707abffc58e386b8969
Persorkki <persorkki@protonmail.com>
2023-03-16 04:06:28 +0200

    formatting

--- src/components/Navigation/LoginButton.js
+++ src/components/Navigation/LoginButton.js
@@ -5,7 +5,11 @@ export default function LoginButton({ login, session }) {
 
     return (
         <>
-            { /* <Link onClick={() => Router.push("/auth/signin?#")} href="#"> */}
+            
+            { /* 
+            TODO: figure out firefox issue or if there is one
+            <Link onClick={() => Router.push("/auth/signin?#")} href="#"> 
+            */}
             <Link onClick={login} href="#">
                 <div className={`${styles.login} ${session ? styles.loggedin : ""}`}>
                     {session ? "logout" : "login"}
--- src/pages/api/upload.js
+++ src/pages/api/upload.js
@@ -1,15 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { PrismaClient } from "@prisma/client";
 import path from 'path'
 import { IncomingForm } from "formidable"
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export const config = {
     api: {
         bodyParser: false,
     }
 }
-
-const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
@@ -26,8 +24,9 @@ export default async function handler(req, res) {
         keepExtensions: true,
         allowEmptyFiles: false,
         // TODO: needs to be env var
-        uploadDir: path.join(process.cwd(), "public"), 
+        uploadDir: path.join(process.cwd(), "public"),
         // default is 200 * 1024 * 1024
+        // TODO: move to a constant (env var?)
         maxFileSize: 5 * 1024 * 1024,
         hashAlgorithm: 'md5',
     }
@@ -50,51 +49,19 @@ export default async function handler(req, res) {
             //TODO: to env...
             remoteFilepath: "http://localhost:3000/" + encodeURI(file.newFilename)
         }
-
-        
+        /*
+        TODO: do we want to compress gifs?
         if (fileObject.mime === "image/gif") {
+            
             // maybe ffmpeg exec call?
             // ffmpeg -i fist.gif -vf "select=eq(n\,0)" -vframes 1 output.png
             return res.status(422).end()
         }
-
-        const newObj = await prisma.file.create({
+        */
+        await prisma.file.create({
             data: fileObject
         }).then(res => console.log(res))
         console.log(fileObject);
-        console.log(newObj);
         return res.status(200).json({ fileObject });
     })
-
-    /*
-    const dbObject = {
-        name: req.body.name,
-        path: req.body.path,
-        tags: req.body.tags
-    }
-
-    await prisma.image.create({
-        data: {
-            name: dbObject.name,
-            path: dbObject.path,
-            tags: {
-                create: dbObject.tags
-              },
-        }
-    })
-    */
-
 }
-/*
-async function upload() {
-    await prisma.image.create({
-        data: {
-            name: "nice image",
-            path: "g:/code/web/sihahdus-necro/public/7.gif",
-            tags: {
-                create: [{ description: "meme" }, { description: "gif" }],
-            }
-        }
-    })
-}
-*/
