74d05b81220939a6ead13fc0ba67f684fe60b1a6
Persorkki <persorkki@protonmail.com>
2023-03-22 19:36:15 +0200

    enabled all logging for prisma even on prod, to figure out the crashes

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,10 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma;
 if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient()
+    prisma = new PrismaClient({
+        log: ['query', 'info', 'warn', 'error'],
+    })
 } else {
     if (!global.prisma) {
-        global.prisma = new PrismaClient()
+        global.prisma = new PrismaClient({
+            log: ['query', 'info', 'warn', 'error'],
+        })
     }
     prisma = global.prisma
 }
