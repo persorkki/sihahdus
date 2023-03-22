2f0f1ced39f3409e7b911237d982844cfb4d19a8
Persorkki <persorkki@protonmail.com>
2023-03-22 17:53:41 +0200

    dunno

--- src/pages/api/message.js
+++ src/pages/api/message.js
@@ -1,7 +1,6 @@
 import prisma from "../../lib/prisma"
 
 export default async function handler(req, res) {
-
     if (req.method === 'GET') {
         const data = await prisma.message.findMany()
         return res.status(200).json({ data })
