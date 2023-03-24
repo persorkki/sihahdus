2f93a4c66b9d45834db9edfcaccb8819858142ad
Persorkki <persorkki@protonmail.com>
2023-03-24 02:27:17 +0200

    ffmpeg converts uploaded gifs to vp9 .webm

--- src/pages/api/upload.js
+++ src/pages/api/upload.js
@@ -1,6 +1,6 @@
 import path from 'path'
 import { IncomingForm } from "formidable"
-
+import { spawn } from 'child_process';
 import prisma from "../../lib/prisma"
 
 const MAX_FILE_SIZE = 50 * 1024 * 1024 // 5M
@@ -11,7 +11,23 @@ export const config = {
     }
 }
 
-
+async function convertGIF(fileObject) {
+    const filenameNoExtension = fileObject.filename.split(".")[0];
+    const cmd = "ffmpeg";
+    const targetLocation = path.join(process.cwd(), "public", `${filenameNoExtension}.webm`)
+    const args = ['-i', fileObject.localFilepath, '-c:v', 'vp9', targetLocation];
+    const ffmpeg = spawn(cmd, args)
+    
+    ffmpeg.stdout.on('data', (data) => {
+        console.log(`data: ${data}`);
+    })
+    ffmpeg.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+      });
+    ffmpeg.on('close', (code) => {
+        console.log(`exit: ${code}`);
+      });
+}
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
@@ -52,15 +68,13 @@ export default async function handler(req, res) {
             localFilepath: file.filepath,
             remoteFilepath: process.env.PUBLIC_FILE_LOCATION  + encodeURI(file.newFilename)
         }
-        /*
-        TODO: do we want to compress gifs?
+        
+        //TODO: do we want to compress gifs?
+        
         if (fileObject.mime === "image/gif") {
-            
-            // maybe ffmpeg exec call?
-            // ffmpeg -i fist.gif -vf "select=eq(n\,0)" -vframes 1 output.png
-            return res.status(422).end()
+            await convertGIF(fileObject)           
         }
-        */
+        
 
         // TODO: docs suggest using try-catch instead of .then .catch
         await prisma.file.create({
