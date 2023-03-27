c7d52a35fadeadc6e7685efdd252e8649bb94dd3
Persorkki <persorkki@protonmail.com>
2023-03-28 02:25:52 +0300

    disable prisma logs for prod

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,15 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
 let prisma;
+let options = { log: ['query', 'info', 'warn', 'error'] }
+
 if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient({
-        log: ['query', 'info', 'warn', 'error'],
-    })
+    prisma = new PrismaClient(
+        //options
+    )
 } else {
     if (!global.prisma) {
-        global.prisma = new PrismaClient({
-            log: ['query', 'info', 'warn', 'error'],
-        })
+        global.prisma = new PrismaClient(
+            options
+        )
     }
     prisma = global.prisma
 }
